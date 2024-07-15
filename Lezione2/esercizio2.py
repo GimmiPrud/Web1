626965aa3785ce5e0a1ca7b01adc79b75ab98df0
GimmiPrud <prudenzig@gmail.c>
2024-07-15 19:07:07 +0200

    ...

--- Lezione2/esercizio2.py
+++ Lezione2/esercizio2.py
@@ -1,30 +1,52 @@
-pag_1  = '''<div class="grid-item">
+pag_1  =  '''
+    <div class="grid-container">
+    <div class="grid-item">
         <div class="row">
             <div class="col">
-              <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+                <img src="image/cpu.png" alt="CPU">
             </div>
             <div class="col">
-              <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+                <p>
+                    <font style = "margin: auto; color: rgb(83, 24, 138);">
+                    <h3>
+                        CPU
+                    </h3>
+                    </font>
+                </p>
             </div>
-          </div>'''
+          </div>
+    </div>'''
 
-pag_2 = '''<div class="grid-item">
+pag_2 = '''  <div class="grid-item">
         <div class="row">
             <div class="col">
-              <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+              <img src="image/scheda_madre.webp" style="height: 30pc;">
             </div>
             <div class="col">
-              <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+                <p>
+                    <font style = "margin: auto; color: rgb(83, 24, 138);">
+                    <h3>
+                        schede madri
+                    </h3>
+                    </font>
+                </p>
             </div>
-          </div>'''
+          </div>
+    </div>'''
 
 pag_3 = '''<div class="grid-item">
         <div class="row">
             <div class="col">
-              <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+                <img src="image/scheda_video.png" alt="schede video" style="height: 30pc;">
             </div>
             <div class="col">
-              <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+                <p>
+                    <font style = "margin: auto; color: rgb(83, 24, 138);">
+                    <h3>
+                        schede video
+                    </h3>
+                    </font>
+                </p>
             </div>
-          </div>'''
-
+          </div>
+    </div>  '''
