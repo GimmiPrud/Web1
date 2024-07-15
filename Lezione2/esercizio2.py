1f6960b14e4d9a45127b1e6f3e5e603b94fa9474
GimmiPrud <prudenzig@gmail.c>
2024-07-15 18:56:13 +0200

    ..

---
+++ Lezione2/esercizio2.py
@@ -0,0 +1,30 @@
+pag_1  = '''<div class="grid-item">
+        <div class="row">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+            </div>
+          </div>'''
+
+pag_2 = '''<div class="grid-item">
+        <div class="row">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+            </div>
+          </div>'''
+
+pag_3 = '''<div class="grid-item">
+        <div class="row">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+            </div>
+          </div>'''
+
