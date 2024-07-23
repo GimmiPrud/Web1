e7b475e0e5ed72a5212237ffe75f40ad8e1fe125
GimmiPrud <prudenzig@gmail.c>
2024-07-23 10:35:45 +0200

    ...

--- Lezione4/server_js/server.js
+++ Lezione4/server_js/server.js
@@ -15,6 +15,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //pagina di invio della form
 app.get('/formRegistrazione', (req, res) => {
-    console.log("Mi hai chiesto la form di registrazione");
-    res.sendFile("formSemplice.html", { root: './htdoc' });
-    });
+console.log("Mi hai chiesto la form di registrazione");
+res.sendFile("formSemplice.html", { root: './htdoc' });
+});
+
+
+//pagina di gestione dei dati della form se il metodo è GET
+app.get('/gestisciDatiForm', (req, res) => {
+console.log(req.query.fname);
+res.send("<html>Buona serata a tutti</html>");
+});
